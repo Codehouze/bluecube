1c0f97932a052f51d56a3a730cee2d990777bae3
Codehouzem <codehouseinc@gmail.com>
2021-11-11 22:00:19 +0100

    update on task

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,17 @@ class Card extends React.Component {
     return (
       <div className="Card">
         {/* create a reusable card component */}
-        
-          <img src={Img} className="Img_holder"></img>
-      
-        <div>
-          <h1>William,28</h1>
-          <p>New york</p>
-        </div>
-        <div className="Card-button">
-          <button className="Button1">Like </button>
-          <button className="Button2">Dislike</button>
+
+        <img src={Img} className="Img_holder"></img>
+        <div className="card_info">
+          <div className="card_name">
+            <h3>William,28</h3>
+            <p>New york</p>
+          </div>
+          <div className="card-button">
+            <button className="Button1">Like </button>
+            <button className="Button2">Dislike</button>
+          </div>
         </div>
       </div>
     );
